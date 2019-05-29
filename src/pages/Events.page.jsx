73a7a84e995a7fa169f9e970feba162babcad026
Perrine Martyris <perrinemartyris@gmail.com>
2019-05-29 16:24:47 +0200

    create the Navbar component and the CSS module

--- src/pages/Events.page.jsx
+++ src/pages/Events.page.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 function Events() {
-  return <div>Events</div>;
+  return (
+    <div>
+      <h1>Events</h1>
+      <Navbar />
+    </div>
+  );
 }
 
 export default Events;
