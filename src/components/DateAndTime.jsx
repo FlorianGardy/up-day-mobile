fc900995fcd2c9bee9e50dea4c27b12a9b5f1881
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-05-29 15:53:56 +0200

    adding DateAndTime component with PropTypes

---
+++ src/components/DateAndTime.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DatePicker, { registerLocale } from "react-datepicker";
+import fr from "date-fns/locale/fr"; // the locale you want
+import "./datepicker.css";
+registerLocale("fr", fr); // register it with the name you want
+
+const DateAndTime = ({ date, handleChange }) => {
+  return (
+    <div>
+      <DatePicker
+        selected={date}
+        onChange={handleChange}
+        showTimeSelect
+        locale="fr"
+        timeFormat="HH:mm"
+        dateFormat="dd MMMM yyyy à HH:mm"
+        withPortal
+      />
+    </div>
+  );
+};
+
+DateAndTime.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  handleChange: PropTypes.func.isRequired
+};
+
+export default DateAndTime;
--- src/pages/Drink.page.jsx
+++ src/pages/Drink.page.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import DateAndTime from "../components/DateAndTime";
 
 function Drink() {
-  return <div>Drink</div>;
+  const [startDate, setStartDate] = useState(new Date());
+  const handleChange = date => {
+    setStartDate(date);
+  };
+  return (
+    <div>
+      <h2>Drink</h2>
+      <DateAndTime date={startDate} handleChange={handleChange} />
+    </div>
+  );
 }
 
 export default Drink;
