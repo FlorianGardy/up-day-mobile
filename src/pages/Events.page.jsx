49bf5918e4c1b58e065778c46ef1c2be766fa30a
Florian Gardy <florian.gardy@gmail.com>
2019-05-29 16:25:48 +0200

    Created Events page Created EventButton component Added 3 EventButton in Events page Created empty Navbar component (for testing purpose)

--- src/pages/Events.page.jsx
+++ src/pages/Events.page.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import EventButton from "../components/EventButton.jsx";
+import Navbar from "../components/Navbar.jsx";
+import { Redirect } from "react-router-dom";
 
-function Events() {
-  return <div>Events</div>;
-}
+const Events = () => {
+  // Component style
+  const style = {
+    groupButton: {
+      padding: "10px",
+      height: "90vh",
+      display: "grid",
+      gridTemplateRows: "33% 33% 33%"
+    }
+  };
+
+  // State definition
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+  const [nextPath, setNextPath] = useState("/events/");
+
+  // Handler functions
+  const handleClick = eventType => {
+    if (eventType === "Boisson") {
+      setNextPath(nextPath + "drink");
+      setShouldRedirect(true);
+    } else if (eventType === "Miction") {
+      setNextPath(nextPath + "urination");
+      setShouldRedirect(true);
+    } else if (eventType === "Défécation") {
+      setNextPath(nextPath + "defecation");
+      setShouldRedirect(true);
+    }
+  };
+
+  // Render
+  return (
+    <div>
+      {shouldRedirect && <Redirect to={nextPath} />}
+      <div>
+        <div style={style.groupButton}>
+          <EventButton
+            text="Boisson"
+            onClick={() => handleClick("Boisson")}
+            backgroundColor="lightblue"
+          />
+          <EventButton
+            text="Miction"
+            onClick={() => handleClick("Miction")}
+            backgroundColor="lightyellow"
+          />
+          <EventButton
+            text="Défécation"
+            onClick={() => handleClick("Défécation")}
+            backgroundColor="lightgreen"
+          />
+        </div>
+        <Navbar />
+      </div>
+    </div>
+  );
+};
 
 export default Events;
