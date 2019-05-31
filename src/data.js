7baf360b803319d114fda054a36018db0f6d1674
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-05-31 17:45:16 +0200

    Creating fake data for events

--- src/data.js
+++ src/data.js
@@ -8,3 +8,36 @@ export const users = [
     password: "1234"
   }
 ];
+
+export const events = [
+  {
+    id: 1,
+    date: "30/06/20",
+    type: "pipi",
+    nature: "normale",
+    volume: "+++",
+    context: "fuite",
+    comment: "pipi",
+    user_id: "1"
+  },
+  {
+    id: 2,
+    date: "30/06/20",
+    type: "pipi",
+    nature: "mitigé",
+    volume: "+",
+    context: "normale",
+    comment: "gros pipi",
+    user_id: "1"
+  },
+  {
+    id: 3,
+    date: "30/06/20",
+    type: "pipi",
+    nature: "mitigé",
+    volume: "++",
+    context: "",
+    comment: "gros",
+    user_id: "1"
+  }
+];
