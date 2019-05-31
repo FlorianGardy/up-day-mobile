2cbdb111ea7d44db4dfe549ff0f0aa85387210ca
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-05-31 18:00:00 +0200

    displayed EventRow in histoy page

--- src/pages/History.page.jsx
+++ src/pages/History.page.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { events } from "../data";
+import EventRow from "../components/EventRow.jsx";
+import Navbar from "../components/Navbar";
 
-function History() {
-  return <div>History</div>;
-}
+const History = () => {
+  //carousselle date + selection date
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  /* state date selectioné */
+  return (
+    <div>
+      {/* replace with topbar */}
+      <h1>Top Bar</h1>
+      {/* replace with component date */}
+      <h2>-- 31/06/20 --</h2>
+      {events
+        // .filter(event => event.date === selectedDate)
+        .map(event => (
+          <EventRow
+            key={event.id}
+            date={event.date}
+            type={event.type}
+            nature={event.nature}
+            volume={event.volume}
+            context={event.context}
+            comment={event.comment}
+          />
+        ))}
+      <Navbar />
+    </div>
+  );
+};
 
 export default History;
