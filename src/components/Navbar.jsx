a7a3a7e0ca3f0543bf08e1eb6dc8aa271c18dc6f
Perrine Martyris <perrinemartyris@gmail.com>
2019-06-03 18:02:32 +0200

    testing scss variables

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-import styles from "./Navbar.module.css";
+import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
   const [shouldDrop, setShouldDrop] = useState(false);
@@ -24,21 +24,21 @@ const Navbar = () => {
       {/* <NavLink to="/events">Nouvel Evenement</NavLink> */}
       <div onClick={handleClick} className={styles.events}>
         Evenement
-        <div className={eventButtonGroup}>
-          <NavLink id="buttonPee" to="/events/urination">
-            Miction
-          </NavLink>
-          <NavLink id="buttonDrink" to="/events/drink">
-            Boisson
-          </NavLink>
-          <NavLink id="buttonPoo" to="/events/defecation">
-            Défécation
-          </NavLink>
-        </div>
       </div>
       <NavLink to="/about" className={styles.about}>
         Infos
       </NavLink>
+      <div className={eventButtonGroup}>
+        <NavLink id="buttonPee" to="/events/urination">
+          Miction
+        </NavLink>
+        <NavLink id="buttonDrink" to="/events/drink">
+          Boisson
+        </NavLink>
+        <NavLink id="buttonPoo" to="/events/defecation">
+          Défécation
+        </NavLink>
+      </div>
     </nav>
   );
 };
