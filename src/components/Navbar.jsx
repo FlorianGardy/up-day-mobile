2b0145967dc4745c160cafb94af87a5a54906fa4
Perrine Martyris <perrinemartyris@gmail.com>
2019-06-03 17:10:30 +0200

    Modified the event button behaviour Added 3 buttons, one per event type Added a function to display/hide the 3 buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [shouldDrop, setShouldDrop] = useState(false);
+
+  const handleClick = () => {
+    setShouldDrop(!shouldDrop);
+  };
+
+  let eventButtonGroup = shouldDrop
+    ? styles.shownEventButtonGroup
+    : styles.hiddenEventButtonGroup;
+  console.log(eventButtonGroup);
+
   return (
     <nav>
-      <ul>
-        <li className={styles.history}>
-          <NavLink to="/history">Historique</NavLink>
-        </li>
-        <li className={styles.events}>
-          <NavLink to="/events">Nouvel Evenement</NavLink>
-        </li>
-        <li className={styles.about}>
-          <NavLink to="/about">Infos</NavLink>
-        </li>
-      </ul>
+      <NavLink to="/history" className={styles.history}>
+        Historique
+      </NavLink>
+      {/* <div className={styles.events}> */}
+      {/* <NavLink to="/events">Nouvel Evenement</NavLink> */}
+      <div onClick={handleClick} className={styles.events}>
+        Evenement
+        <div className={eventButtonGroup}>
+          <NavLink id="buttonPee" to="/events/urination">
+            Miction
+          </NavLink>
+          <NavLink id="buttonDrink" to="/events/drink">
+            Boisson
+          </NavLink>
+          <NavLink id="buttonPoo" to="/events/defecation">
+            Défécation
+          </NavLink>
+        </div>
+      </div>
+      <NavLink to="/about" className={styles.about}>
+        Infos
+      </NavLink>
     </nav>
   );
 };
