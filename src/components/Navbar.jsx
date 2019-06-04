c55efbf468f439c45f10d5c292e7c7b9e31af1af
Perrine Martyris <perrinemartyris@gmail.com>
2019-06-04 12:27:24 +0200

    finished the new EventButton behaviour and style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,29 +13,26 @@ const Navbar = () => {
   let eventButtonGroup = shouldDrop
     ? styles.shownEventButtonGroup
     : styles.hiddenEventButtonGroup;
-  console.log(eventButtonGroup);
 
   return (
-    <nav>
+    <nav className={styles.bottomNavBar}>
       <NavLink to="/history" className={styles.history}>
         Historique
       </NavLink>
-      {/* <div className={styles.events}> */}
-      {/* <NavLink to="/events">Nouvel Evenement</NavLink> */}
       <div onClick={handleClick} className={styles.events}>
-        Evenement
+        +
       </div>
       <NavLink to="/about" className={styles.about}>
         Infos
       </NavLink>
       <div className={eventButtonGroup}>
-        <NavLink id="buttonPee" to="/events/urination">
+        <NavLink id={styles.buttonPee} to="/events/urination">
           Miction
         </NavLink>
-        <NavLink id="buttonDrink" to="/events/drink">
+        <NavLink id={styles.buttonDrink} to="/events/drink">
           Boisson
         </NavLink>
-        <NavLink id="buttonPoo" to="/events/defecation">
+        <NavLink id={styles.buttonPoo} to="/events/defecation">
           Défécation
         </NavLink>
       </div>
