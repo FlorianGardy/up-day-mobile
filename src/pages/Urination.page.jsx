912387248dc66eeee827f5f1b7a7dcfb01473819
Your Name <ns8@hotmail.fr>
2019-06-05 12:17:26 +0200

    adding navbar in urination page

--- src/pages/Urination.page.jsx
+++ src/pages/Urination.page.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 function Urination() {
-  return <div>Urination</div>;
+  return (
+    <div>
+      <h1>Urination</h1>
+      <Navbar />
+    </div>
+  );
 }
 
 export default Urination;
