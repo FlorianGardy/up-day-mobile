340d6598ba6ab025860f58782ec2535239ac01bb
Florian Gardy <florian.gardy@gmail.com>
2019-06-11 13:24:01 +0200

    Created getUserEventsFromAPI function The function just calls the API (/events with {param: userID}).

--- src/API/functions.js
+++ src/API/functions.js
@@ -1,4 +1,5 @@
 import { users } from "../data";
+import Axios from "axios";
 
 // Check if the user / pass exists in the database
 export const isUserInDatabase = (username, password) => {
@@ -9,4 +10,28 @@ export const isUserInDatabase = (username, password) => {
     }
   });
   return result;
+};
+
+// Get all the events of a given user from the API
+export const getUserEventsFromAPI = userID => {
+  const config = {
+    method: "get",
+    baseURL: "http://localhost:3001/",
+    url: "/events",
+    params: {
+      userID: userID
+    }
+  };
+
+  Axios.request(config)
+    .then(function(response) {
+      // handle success
+      console.log(response);
+      const events = response.data.events;
+      console.log(events);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    });
 };
