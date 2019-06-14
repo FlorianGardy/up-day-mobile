06c62349a5c179a9f5c905bc9a47b3cc68830fbb
Perrine Martyris <perrinemartyris@gmail.com>
2019-06-14 11:04:27 +0200

    navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { resetEvent } from "../pills/event/event.action";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestion } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
 
 import styles from "./Navbar.module.scss";
 
@@ -17,24 +20,26 @@ const Navbar = ({ dispatch }) => {
     : styles.hiddenEventButtonGroup;
 
   return (
-    <nav className={styles.bottomNavBar}>
-      <NavLink to="/history" className={styles.history}>
-        Historique
-      </NavLink>
-      <div onClick={handleClick} className={styles.events}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="40"
-          height="40"
-          viewBox="0 0 24 24"
-          className={styles.cross}
-        >
-          <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
-        </svg>
+    <nav className={styles.navBar}>
+      <div className={styles.bottomNavBar}>
+        <NavLink to="/history" className={styles.history}>
+          <FontAwesomeIcon icon={faCalendarAlt} size="2x" />
+        </NavLink>
+        <div onClick={handleClick} className={styles.events}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            className={styles.cross}
+          >
+            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
+          </svg>
+        </div>
+        <NavLink to="/about" className={styles.about}>
+          <FontAwesomeIcon icon={faQuestion} size="2x" />
+        </NavLink>
       </div>
-      <NavLink to="/about" className={styles.about}>
-        Infos
-      </NavLink>
       <div
         className={eventButtonGroup}
         onClick={() => {
