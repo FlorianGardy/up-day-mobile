b7d8087cc6654ced39c0169ab3ac9523a0ffd7f1
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-06-17 14:20:57 +0200

    adding title in props for selectors components

--- src/components/ContextSelector.jsx
+++ src/components/ContextSelector.jsx
@@ -2,29 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./ContextSelector.module.css";
 
-const ContextSelector = ({ options, onChange, context }) => {
+const ContextSelector = ({ options, onChange, context, title }) => {
   return (
-    <div>
-      {options.map((option, i) => {
-        return (
-          <div key={i} style={{ margin: "2px" }}>
-            <label className={styles.switch}>
-              <input
-                type="checkbox"
-                checked={context.map(el => el.value).includes(option.label)}
-                onChange={e => onChange(option.label, e.target.checked)}
-              />
-              <span className={[styles.slider, styles.round].join(" ")} />
-            </label>
-            {option.label}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div>
+        <h2>{title}</h2>
+        {options.map((option, i) => {
+          return (
+            <div key={i} style={{ margin: "2px" }}>
+              <label className={styles.switch}>
+                <input
+                  type="checkbox"
+                  checked={context.map(el => el.value).includes(option.label)}
+                  onChange={e => onChange(option.label, e.target.checked)}
+                />
+                <span className={[styles.slider, styles.round].join(" ")} />
+              </label>
+              {option.label}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
 ContextSelector.propTypes = {
+  title: PropTypes.string.isRequired,
   options: PropTypes.array.isRequired,
   context: PropTypes.array.isRequired,
   onChange: PropTypes.func.isRequired
--- src/components/OptionSelector.jsx
+++ src/components/OptionSelector.jsx
@@ -2,29 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./OptionSelector.module.css";
 
-const OptionSelector = ({ options, onClick, activeOption }) => {
+const OptionSelector = ({ options, onClick, activeOption, title }) => {
   return (
-    <div>
-      {options.map((option, i) => {
-        let styleButton = styles.button;
-        if (activeOption === option.value) {
-          styleButton = styles.selectedButton;
-        }
-        return (
-          <button
-            className={styleButton}
-            key={i}
-            onClick={() => onClick(option.value)}
-          >
-            {option.label}
-          </button>
-        );
-      })}
-    </div>
+    <>
+      <h2>{title}</h2>
+      <div>
+        {options.map((option, i) => {
+          let styleButton = styles.button;
+          if (activeOption === option.value) {
+            styleButton = styles.selectedButton;
+          }
+          return (
+            <button
+              className={styleButton}
+              key={i}
+              onClick={() => onClick(option.value)}
+            >
+              {option.label}
+            </button>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
 OptionSelector.propTypes = {
+  title: PropTypes.string.isRequired,
   options: PropTypes.array.isRequired,
   activeOption: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired
