0605b80bd2e1b93c09bb7cb5cedc0873fb6e7b10
Perrine Martyris <perrinemartyris@gmail.com>
2019-06-26 11:02:31 +0200

    added the history page for the first connection

---
+++ src/components/HistoryFirstCo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HistoryFirstCo = () => {
+  return (
+    <div className="historyFirstCo">
+      <h2>Bienvenue !</h2>
+      <p>
+        Pour démarrer voter calendrier, appuyez sur le boutton + en bas de votre
+        écran. <br />
+        Choisissez ensuite l'évènement que vous voulez enregistrer et renseignez
+        les informations demandées.
+      </p>
+    </div>
+  );
+};
+
+export default HistoryFirstCo;
--- src/pills/history/history.view.jsx
+++ src/pills/history/history.view.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import EventRow from "../../components/EventRow";
 import moment from "moment";
 import "moment/locale/fr";
+import HistoryFirstCo from "../../components/HistoryFirstCo";
 
 const HistoryView = ({ history }) => {
   return (
-    <div className="eventList">
-      {history.reverse().map(event => (
-        <EventRow
-          key={event.id}
-          date={moment(event.date).format("HH:mm")}
-          type={event.type}
-          nature={event.nature}
-          volume={event.volume}
-          context={event.context}
-          comment={event.comment}
-        />
-      ))}
-    </div>
+    <>
+      {history.length === 0 ? (
+        <HistoryFirstCo />
+      ) : (
+        history
+          .reverse()
+          .map(event => (
+            <EventRow
+              key={event.id}
+              date={moment(event.date).format("HH:mm")}
+              type={event.type}
+              nature={event.nature}
+              volume={event.volume}
+              context={event.context}
+              comment={event.comment}
+            />
+          ))
+      )}
+    </>
   );
 };
 
